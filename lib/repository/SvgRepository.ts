3595d9aaa85e1a8216508e71ca8fd38ed31943e5
potados99 <potados99@gmail.com>
2021-01-31 11:59:55 +0900

    Color!

--- lib/repository/SvgRepository.ts
+++ lib/repository/SvgRepository.ts
@@ -8,10 +8,17 @@ class SvgRepository {
             fill="none"
             xmlns="http://www.w3.org/2000/svg">
 
+            <defs>
+                <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+                    <stop offset="0%" style="stop-color: rgb(89,188,227); stop-opacity: 1" />
+                    <stop offset="100%" style="stop-color: rgb(72,52,198); stop-opacity: 1" />
+                </linearGradient>
+            </defs>
+        
             <style>
                 .header {
                     font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif;
-                    fill: #222222;
+                    fill: url(#grad1);
                     animation: fadeInAnimation 0.8s ease-in-out forwards;
                 }
 
