a11914061a2523b1dd6856f0dc815cdcd000b170
potados99 <potados99@gmail.com>
2021-01-31 11:08:01 +0900

    Handle API limit!

--- lib/repository/ZenRepository.ts
+++ lib/repository/ZenRepository.ts
@@ -27,7 +27,37 @@ class ZenRepository {
 
     private async fetchZen() {
         const result = await fetch(config.zen.sourceUrl);
-        return result.text();
+        const content = await result.text();
+        const isAnError = content.startsWith("{");
+
+        if (isAnError) {
+            console.log(`Error! ${content}`);
+            return this.getFromFallbackZen();
+        } else {
+            return content;
+        }
+    }
+
+    private getFromFallbackZen() {
+        const allZens = [
+            "It's not fully shipped until it's fast.",
+            "Practicality beats purity.",
+            "Avoid administrative distraction.",
+            "Mind your words, they are important.",
+            "Non-blocking is better than blocking.",
+            "Design for failure.",
+            "Half measures are as bad as nothing at all.",
+            "Favor focus over features.",
+            "Approachable is better than simple.",
+            "Encourage flow.",
+            "Anything added dilutes everything else.",
+            "Speak like a human.",
+            "Responsive is better than fast.",
+            "Keep it logically awesome."
+        ];
+
+        // Random pick
+        return allZens[Math.floor(Math.random() * allZens.length)];
     }
 }
 
